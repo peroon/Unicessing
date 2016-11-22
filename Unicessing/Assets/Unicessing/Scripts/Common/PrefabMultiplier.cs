8eb67b0f1e369cd3eaeb151e1179db41d982a190
peroon <fromjpn@gmail.com>
2016-11-22 23:50:16 +0900

    Add a sample of random color & not frame clear

---
+++ Unicessing/Assets/Unicessing/Scripts/Common/PrefabMultiplier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabMultiplier : MonoBehaviour {
+
+	public GameObject prefab;
+	public int num;
+
+	void Start () {
+		for (int i = 0; i < num; i++) {
+			var go = Instantiate (prefab, transform.position, Quaternion.identity) as GameObject;
+			go.GetComponent<Renderer> ().material.color = ColorUtil.RandomColor (); 
+		}	
+	}
+	
+}
---
+++ Unicessing/Assets/Unicessing/Scripts/Common/Util/ColorUtil.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorUtil : MonoBehaviour {
+
+	public static Color RandomColor(){
+		return new Color(Random.value, Random.value, Random.value);
+	}
+
+}
