460c07dd4d1723053ee9e4318b01b0f0044e1bed
peroon <fromjpn@gmail.com>
2016-11-22 23:30:57 +0900

    Add sample that don't clear each frame

---
+++ Unicessing/Assets/Unicessing/Scripts/Common/OverdrawCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverdrawCamera : MonoBehaviour {
+
+	// クリア方法
+	public CameraClearFlags clearFlag = CameraClearFlags.Depth;
+	
+	void Start () {
+		Camera.main.clearFlags = clearFlag;
+	}
+}
